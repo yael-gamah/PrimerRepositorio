2a27ae9db0c9f577dd5279697179773fbb515a4e
Yael GH <yaelhernandezg123@gmail.com>
2024-09-18 11:59:05 -0600

    Método razaPerro

--- src/Perro/Perro.java
+++ src/Perro/Perro.java
@@ -3,5 +3,13 @@ package Perro;
 public class Perro{
 public static void main(String[] args) {
         System.out.println("Guau");
+        System.out.println(razaPerro("Salchicha"));
+    }
+
+
+    public static String razaPerro(String n){
+        return "Soy un perro de raza: "+n;
     }
 }
+
+
