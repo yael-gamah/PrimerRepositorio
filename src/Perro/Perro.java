b02c0736eb5e8f3bd7293c23fa2aad21ac6b3eb7
areli-sanchez28 <jals281202@gmail.com>
2024-09-18 12:41:38 -0600

    Primer Commit Arelilla

--- src/Perro/Perro.java
+++ src/Perro/Perro.java
@@ -1,15 +1,29 @@
 package Perro;
 
-public class Perro{
-public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Perro {
+
+    public static void main(String[] args) {
         System.out.println("Guau");
         System.out.println(razaPerro("Salchicha"));
     }
 
+    public static String razaPerro(String n) {
+        return "Soy un perro de raza: " + n;
+    }
 
-    public static String razaPerro(String n){
-        return "Soy un perro de raza: "+n;
+    public static void nombrePerro() {
+
+        // Pedir al usuario que ingrese el nombre del perro
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Pedir al usuario que ingrese el nombre del perro
+            System.out.print("Introduce el nombre del perro: ");
+            String nombre = scanner.nextLine();
+            
+            // Mostrar el nombre del perro
+            System.out.println("El nombre del perro es: " + nombre);
+        }
+
     }
 }
-
-
